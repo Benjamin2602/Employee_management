0b2e671b0f2d91be0855cb49f1084783bf7b21b5
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-12-11 13:40:36 +0530

    UseState applied

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
 const Home = () => {
+    const [name, setName] = useState("");
+    const [department, setDepartment] = useState("");
+    const [salary, setSalary] = useState(0);
+    const [designation, setDesignation] = useState("");
+    const [dob, setDob] = useState("");
+    const [address, setAddress] = useState("");
+
   return (
     <div className="flex flex-col justify-center">
       <div className="flex flex-col p-2.5 items-center">
@@ -9,6 +17,7 @@ const Home = () => {
           type="text"
           id="name"
           placeholder="Enter your name"
+          onChange={(e) => setName(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <label className="text-lg font-bold">Department</label>
@@ -16,6 +25,7 @@ const Home = () => {
           type="text"
           id="department"
           placeholder="Enter your department"
+          onChange={(e) => setDepartment(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <label className="text-lg font-bold">Salary</label>
@@ -23,6 +33,7 @@ const Home = () => {
           type="number"
           id="salary"
           placeholder="Enter your salary"
+          onChange={(e) => setSalary(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <label className="text-lg font-bold">designation</label>
@@ -30,6 +41,7 @@ const Home = () => {
           type="text"
           id="age"
           placeholder="Enter your designation"
+          onChange={(e) => setDesignation(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <label className="text-lg font-bold">dob</label>
@@ -37,6 +49,7 @@ const Home = () => {
           type="text"
           id="dob"
           placeholder="Enter your dob"
+          onChange={(e) => setDob(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <label className="text-lg font-bold">Address</label>
@@ -44,6 +57,7 @@ const Home = () => {
           type="text"
           id="address"
           placeholder="Enter your address"
+          onChange={(e) => setAddress(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
         <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-3">
