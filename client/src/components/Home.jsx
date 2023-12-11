13dd476288becbfdc8dfebd6b6f25a2cc9800c0a
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-12-11 14:12:26 +0530

    constraints(alert msg to fill all fields)

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,27 +1,37 @@
 import { useState } from "react";
 import Axios from "axios";
 const Home = () => {
-    const [name, setName] = useState("");
-    const [department, setDepartment] = useState("");
-    const [salary, setSalary] = useState(0);
-    const [designation, setDesignation] = useState("");
-    const [dob, setDob] = useState("");
-    const [address, setAddress] = useState("");
+  const [name, setName] = useState("");
+  const [department, setDepartment] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [designation, setDesignation] = useState("");
+  const [dob, setDob] = useState("");
+  const [address, setAddress] = useState("");
 
-    const addEmployee = () => {
-        console.log(name);
-        Axios.post("http://localhost:3000/create", {
-            name: name,
-            department: department,
-            salary: salary,
-            designation: designation,
-            dob: dob,
-            address: address,
-        }).then(() => {
-            console.log("successfully added");
-        });
-
-    };
+  const addEmployee = () => {
+    console.log(name);
+    if (
+      name === "" ||
+      department === "" ||
+      salary === "" ||
+      designation === "" ||
+      dob === "" ||
+      address === ""
+    ) {
+      alert("Please fill all the fields");
+      return;
+    }
+    Axios.post("http://localhost:3000/create", {
+      name: name,
+      department: department,
+      salary: salary,
+      designation: designation,
+      dob: dob,
+      address: address,
+    }).then(() => {
+      console.log("successfully added");
+    });
+  };
 
   return (
     <div className="flex flex-col justify-center">
@@ -76,8 +86,10 @@ const Home = () => {
           onChange={(e) => setAddress(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
-        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-3"
-        onClick={addEmployee}>
+        <button
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-3"
+          onClick={addEmployee}
+        >
           Add Employee
         </button>
       </div>
