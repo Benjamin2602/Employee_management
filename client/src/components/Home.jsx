a0342d626761293e0e06bcbddceea76b5edbe052
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-12-11 14:10:54 +0530

    Inserting data from client to DB

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Axios from "axios";
 const Home = () => {
     const [name, setName] = useState("");
     const [department, setDepartment] = useState("");
@@ -6,6 +7,21 @@ const Home = () => {
     const [designation, setDesignation] = useState("");
     const [dob, setDob] = useState("");
     const [address, setAddress] = useState("");
+
+    const addEmployee = () => {
+        console.log(name);
+        Axios.post("http://localhost:3000/create", {
+            name: name,
+            department: department,
+            salary: salary,
+            designation: designation,
+            dob: dob,
+            address: address,
+        }).then(() => {
+            console.log("successfully added");
+        });
+
+    };
 
   return (
     <div className="flex flex-col justify-center">
@@ -60,7 +76,8 @@ const Home = () => {
           onChange={(e) => setAddress(e.target.value)}
           className="h-9 w-1/3 border border-gray-400 rounded-md"
         />
-        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-3">
+        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-3"
+        onClick={addEmployee}>
           Add Employee
         </button>
       </div>
--- server/server.js
+++ server/server.js
@@ -12,17 +12,44 @@ app.use(express.json());
 
 //connecting to the mySql database
 const db = mysql.createConnection({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
+  user: "root",
+  host: "localhost",
+  password: process.env.DB_PASSWORD,
+  database: "employeedemo",
 });
 
 db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to MySQL:", err);
-      return;
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+//creating a new user
+app.post("/create", (req, res) => {
+  console.log("received request ", req.body);
+  const name = req.body.name;
+  const department = req.body.department;
+  const salary = req.body.salary;
+  const designation = req.body.designation;
+  const dob = req.body.dob;
+  const address = req.body.address;
+
+  //inserting the user into the database
+  db.query(
+    "INSERT INTO employees (name, department, salary, designation, dob, address) VALUES (?,?,?,?,?,?)",
+    [name, department, salary, designation, dob, address],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values inserted");
+      }
     }
-    console.log("Connected to MySQL");
-  });
-  
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
